6976238ea1ed4b928b112a771f32d89b759a53ad
PratikJadhav <pratikjadhav7663@gmail.com>
2025-06-30 15:29:52 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import AboutPage from "./pages/AboutPage";
 import Contactpage from "./pages/ContactPage";
 import CoursesPage from "./pages/CoursesPage";
 import "./styles/Pages.css";
-import "./App.css";
+// import "./App.css";
 import AdmissionPages from "./pages/AdmissionPage";
 import ChatbotComponent from "./components/Chatbot/ChatbotComponents";
 import Header from "./components/Header/Header";
